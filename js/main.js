c141afc323335c0a66bbffc3d25b862ebacb410e
Matias Lofiego <matiaslofiego@gmail>
2023-04-26 18:00:20 -0300

    implementación de js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let apellido;
+let edad;
+let direccion;
+apellido="Perez"; //string
+edad=30; //number
+direccion= "Bolivia 123";
+
+let dia= "Lunes";
+let nombre= "Matias";
+apellido= "Ais";
+
+const PI=3.14; //constantes
+const IVA =21;
+const GRAVEDAD =9.8;
+
+let resultado= PI + 4;
+let gravedadXiva= GRAVEDAD * IVA;
+let nombreCompleto= nombre + apellido //concatenación de variables
+
+console.log("Buenos dias mandy");
